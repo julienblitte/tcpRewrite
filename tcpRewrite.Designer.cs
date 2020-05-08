c7b612c7c016dd02ce16ccdf149b8cbdad52a53c
Julien Blitte <julien.blitte@gmail.com>
2020-05-09 00:23:43 +0200

    Initial commit

---
+++ tcpRewrite.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace tcpRwrite
+{
+    partial class tcpRewrite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tcpRewrite));
+            this.destPort = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.destIP = new System.Windows.Forms.TextBox();
+            this.toggle = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fromPort = new System.Windows.Forms.NumericUpDown();
+            this.replacementsGrid = new System.Windows.Forms.DataGridView();
+            this.Search = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Replace = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.way = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fromIP = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.destPort)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fromPort)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.replacementsGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // destPort
+            // 
+            this.destPort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.destPort.Location = new System.Drawing.Point(476, 57);
+            this.destPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.destPort.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.destPort.Name = "destPort";
+            this.destPort.Size = new System.Drawing.Size(76, 20);
+            this.destPort.TabIndex = 4;
+            this.destPort.Value = new decimal(new int[] {
+            6554,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Forward to";
+            // 
+            // destIP
+            // 
+            this.destIP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.destIP.Location = new System.Drawing.Point(76, 57);
+            this.destIP.Name = "destIP";
+            this.destIP.Size = new System.Drawing.Size(394, 20);
+            this.destIP.TabIndex = 3;
+            this.destIP.Text = "127.0.0.1";
+            // 
+            // toggle
+            // 
+            this.toggle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.toggle.Location = new System.Drawing.Point(216, 381);
+            this.toggle.Name = "toggle";
+            this.toggle.Size = new System.Drawing.Size(145, 39);
+            this.toggle.TabIndex = 6;
+            this.toggle.Text = "Start";
+            this.toggle.UseVisualStyleBackColor = true;
+            this.toggle.Click += new System.EventHandler(this.toggle_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Listen from";
+            // 
+            // fromPort
+            // 
+            this.fromPort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fromPort.Location = new System.Drawing.Point(476, 22);
+            this.fromPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.fromPort.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.fromPort.Name = "fromPort";
+            this.fromPort.Size = new System.Drawing.Size(76, 20);
+            this.fromPort.TabIndex = 2;
+            this.fromPort.Value = new decimal(new int[] {
+            554,
+            0,
+            0,
+            0});
+            // 
+            // replacementsGrid
+            // 
+            this.replacementsGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.replacementsGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.replacementsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.replacementsGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Search,
+            this.Replace,
+            this.way});
+            this.replacementsGrid.Location = new System.Drawing.Point(15, 116);
+            this.replacementsGrid.Name = "replacementsGrid";
+            this.replacementsGrid.Size = new System.Drawing.Size(537, 248);
+            this.replacementsGrid.TabIndex = 5;
+            // 
+            // Search
+            // 
+            this.Search.HeaderText = "Regex";
+            this.Search.Name = "Search";
+            // 
+            // Replace
+            // 
+            this.Replace.HeaderText = "Replace";
+            this.Replace.Name = "Replace";
+            // 
+            // way
+            // 
+            this.way.HeaderText = "Way";
+            this.way.Items.AddRange(new object[] {
+            "Both",
+            "Forward",
+            "Backward",
+            "None"});
+            this.way.Name = "way";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Replace text";
+            // 
+            // fromIP
+            // 
+            this.fromIP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fromIP.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.fromIP.FormattingEnabled = true;
+            this.fromIP.Location = new System.Drawing.Point(76, 21);
+            this.fromIP.Name = "fromIP";
+            this.fromIP.Size = new System.Drawing.Size(394, 21);
+            this.fromIP.TabIndex = 14;
+            // 
+            // tcpRewrite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 436);
+            this.Controls.Add(this.fromIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.replacementsGrid);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.fromPort);
+            this.Controls.Add(this.toggle);
+            this.Controls.Add(this.destIP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.destPort);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "tcpRewrite";
+            this.Text = "TCP rewrite";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.tcpRewrite_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.destPort)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fromPort)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.replacementsGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown destPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox destIP;
+        private System.Windows.Forms.Button toggle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown fromPort;
+        private System.Windows.Forms.DataGridView replacementsGrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Search;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Replace;
+        private System.Windows.Forms.DataGridViewComboBoxColumn way;
+        private System.Windows.Forms.ComboBox fromIP;
+    }
+}
+
---
+++ tcpRewrite.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tcpRwrite
+{
+    public partial class tcpRewrite : Form
+    {
+        private bool status;
+
+        private TcpListener localServer; // will listen
+        private TcpClient distantServer;   // will connect to real RTSP server
+
+        private TcpClient localServerClient;  // client connected to middle server
+
+        private NetworkStream localServerClientStream, distantServerStream;
+
+        private Thread mainThread;
+
+        private byte[] fromLocalServerClient, fromDistantServer;
+
+
+        private struct replacementEntry
+        {
+            public Regex regex;
+            public string replacement;
+            public bool forward;
+            public bool backward;
+        }
+
+        private replacementEntry[] replacements;
+
+        public tcpRewrite()
+        {
+            InitializeComponent();
+            status = false;
+        }
+
+        private void mainThreadProc(object sender)
+        {
+            //Thread clientToServerThread, serverToClientThread;
+
+            fromLocalServerClient = new byte[1500];
+            fromDistantServer = new byte[1500];
+
+            while (status)
+            {
+                try
+                {
+                    localServerClient = localServer.AcceptTcpClient();
+                }
+                catch
+                {
+                    if (!status)
+                        break;
+                }
+                Trace.WriteLine(String.Format("New client connected: {0}", localServerClient.Client.RemoteEndPoint.ToString()));
+                localServerClientStream = localServerClient.GetStream();
+
+                try
+                {
+                    distantServer = new TcpClient(destIP.Text, (UInt16)destPort.Value);
+                }
+                catch
+                {
+                    Trace.WriteLine(String.Format("Error: Unable to establish connection to: {0}:{1}", destIP.Text, (UInt16)destPort.Value));
+                    break;
+                }
+                Trace.WriteLine(String.Format("Connected to: {0}", distantServer.Client.RemoteEndPoint.ToString()));
+                distantServerStream = distantServer.GetStream();
+
+                localServerClientStream.BeginRead(fromLocalServerClient, 0, fromLocalServerClient.Length, localServerClientStreamRead, null);
+                distantServerStream.BeginRead(fromDistantServer, 0, fromDistantServer.Length, distantServerStreamRead, null);
+            }
+            ((tcpRewrite)sender).stop();
+        }
+
+        string processReplacement(string text, bool forward, bool backward)
+        {
+            for (int i = 0; i < replacements.Length; i++)
+            {
+                if ((replacements[i].forward && forward)
+                || (replacements[i].backward && backward))
+                {
+                    text = replacements[i].regex.Replace(text, replacements[i].replacement);
+                }
+            }
+            return text;
+        }
+
+        void localServerClientStreamRead(IAsyncResult ar)
+        {
+            int read;
+            string text;
+            byte[] output;
+
+            if (!status)
+                return;
+
+            try
+            {
+                read = localServerClientStream.EndRead(ar);
+                text = Encoding.UTF8.GetString(fromLocalServerClient, 0, read);
+                text = processReplacement(text, true, false);
+                output = Encoding.UTF8.GetBytes(text);
+
+                if (output.Length != 0)
+                {
+                    distantServerStream.Write(output, 0, output.Length);
+                    Trace.WriteLine(String.Format("{0}({4} bytes) {1}\r\n{2}\r\n{3}\r\n", "client", "{", text, "}", text.Length));
+                }
+
+                localServerClientStream.BeginRead(fromLocalServerClient, 0, fromLocalServerClient.Length, localServerClientStreamRead, null);
+            }
+            catch
+            {
+                Trace.WriteLine("Connexion closed.");
+                stop();
+                return;
+            }
+        }
+
+        void distantServerStreamRead(IAsyncResult ar)
+        {
+            int read;
+            string text;
+            byte[] output;
+
+            if (!status)
+                return;
+
+            try
+            {
+                read = distantServerStream.EndRead(ar);
+                text = Encoding.UTF8.GetString(fromDistantServer, 0, read);
+                text = processReplacement(text, false, true);
+                output = Encoding.UTF8.GetBytes(text);
+
+                if (output.Length != 0)
+                {
+                    localServerClientStream.Write(output, 0, output.Length);
+                    Trace.WriteLine(String.Format("{0}({4} bytes) {1}\r\n{2}\r\n{3}\r\n", "server", "{", text, "}", text.Length));
+                }
+
+                distantServerStream.BeginRead(fromDistantServer, 0, fromDistantServer.Length, distantServerStreamRead, null);
+            }
+            catch
+            {
+                Trace.WriteLine("Connexion closed.");
+                stop();
+                return;
+            }
+        }
+
+        private void stop()
+        {
+            status = false;
+
+            if (distantServer != null)
+            {
+                distantServer.Close();
+            }
+
+            if (localServerClient != null)
+            {
+                localServerClient.Close();
+            }
+
+            if (localServer != null)
+            {
+                localServer.Stop();
+            }
+
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(() => { updateGUI(); }));
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            stop();
+        }
+
+        private void loadReplacements()
+        {
+            List<replacementEntry> entries;
+
+            entries = new List<replacementEntry>();
+
+            foreach (DataGridViewRow row in replacementsGrid.Rows)
+            {
+                replacementEntry entry;
+
+                entry = new replacementEntry();
+
+                if (row.Cells[0].Value == null)
+                    continue;
+
+                try
+                {
+                    entry.regex = new Regex(row.Cells[0].Value.ToString());
+                }
+                catch
+                {
+                    continue;
+                }
+
+                entry.replacement = row.Cells[1].Value.ToString();
+                switch (row.Cells[2].Value.ToString())
+                {
+                    case "Forward":
+                        entry.forward = true;
+                        entry.backward = false;
+                        break;
+                    case "Backward":
+                        entry.backward = true;
+                        entry.forward = false;
+                        break;
+                    case "Both":
+                        entry.forward = true;
+                        entry.backward = true;
+                        break;
+                    default:
+                    case "None":
+                        entry.forward = false;
+                        entry.backward = false;
+                        break;
+                }
+                entries.Add(entry);
+            }
+
+            replacements = entries.ToArray();
+        }
+
+        private void start()
+        {
+            status = true;
+            updateGUI();
+
+            loadReplacements();
+
+            try
+            {
+                localServer = new TcpListener(IPAddress.Parse(fromIP.Text), (int)fromPort.Value);
+                localServer.Start();
+
+                mainThread = new Thread(mainThreadProc);
+                mainThread.Start(this);
+            }
+            catch
+            {
+                MessageBox.Show(String.Format("Unable to listen on {0}:{1}!", fromIP.Text, fromPort.Value), "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                stop();
+            }
+        }
+
+        protected NetworkInterface[] listActiveInterface()
+        {
+            var interfaceList =
+                from iface in NetworkInterface.GetAllNetworkInterfaces()
+                where iface.OperationalStatus == OperationalStatus.Up
+                select iface;
+
+            return interfaceList.Cast<NetworkInterface>().ToArray();
+        }
+
+        protected IPAddress[] listInterfaceAddresses(NetworkInterface iface, AddressFamily adressType)
+        {
+            var addressList =
+                from addr in iface.GetIPProperties().UnicastAddresses
+                where (addr.Address.AddressFamily == adressType)
+                select addr.Address;
+
+            return addressList.Cast<IPAddress>().ToArray();
+        }
+
+
+        private void tcpRewrite_Load(object sender, EventArgs e)
+        {
+            fromIP.Items.Add("0.0.0.0");
+
+            var nicList = listActiveInterface();
+            foreach(var nic in nicList)
+            {
+                var addrList = listInterfaceAddresses(nic, AddressFamily.InterNetwork);
+                foreach(var addr in addrList)
+                {
+                    fromIP.Items.Add(addr.ToString());
+                }
+            }
+
+            fromIP.SelectedIndex = 0;
+        }
+
+        private void toggle_Click(object sender, EventArgs e)
+        {
+            if (status)
+            {
+                stop();
+            }
+            else
+            {
+                start();
+            }
+        }
+
+        private void updateGUI()
+        {
+            toggle.Text = (status ? "Stop" : "Start");
+
+            replacementsGrid.ReadOnly = status;
+        }
+    }
+}
